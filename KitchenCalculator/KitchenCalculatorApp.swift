176a3946a7eb4cf080724b705a4f5ca7f693a601
KaMaDi <artkamadi@gmail.com>
2024-05-14 19:31:46 +0300

    Initial Commit

---
+++ KitchenCalculator/KitchenCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  KitchenCalculatorApp.swift
+//  KitchenCalculator
+//
+//  Created by Jedi on 14.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct KitchenCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
