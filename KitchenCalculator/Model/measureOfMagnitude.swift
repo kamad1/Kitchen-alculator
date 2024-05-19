17f614330620707fc7f5ec3c07528659e1c129d3
KaMaDi <artkamadi@gmail.com>
2024-05-19 14:07:06 +0300

    hh

--- KitchenCalculator/Model/measureOfMagnitude.swift
+++ KitchenCalculator/Model/measureOfMagnitude.swift
@@ -7,10 +7,14 @@ struct measureOfMagnitude: Identifiable, Hashable {
     var name: String
     var measureValue: Bool = false
     
-    var reserchValue: Int = {
-        var e = 0
-        return e
-    }()
+//    var reserchValue: Int = {
+//        var result = 0
+//        switch PickerModelGramm {
+//        case .gramm: return result / 1000
+//        case .litr: return result / 1000
+//        }
+//        return result
+//    }()
     
 }
 
